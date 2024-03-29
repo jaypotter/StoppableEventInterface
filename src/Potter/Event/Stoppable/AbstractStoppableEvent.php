dde489d36cd0f5db22ec44b9b5f89f7471885d9d
Jay Potter <j@ypotter.ca>
2024-03-29 09:56:31 -0400

    name correction

--- src/Potter/Event/Stoppable/AbstractStoppableEvent.php
+++ src/Potter/Event/Stoppable/AbstractStoppableEvent.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Potter\Event\Stoppable;
 
-abstract class AbstractStoppableEventInterface implements StoppableEventInterface
+abstract class AbstractStoppableEvent implements StoppableEventInterface
 {
     abstract public function isPropagationStopped(): bool;
     abstract protected function stopPropagation(): void;
